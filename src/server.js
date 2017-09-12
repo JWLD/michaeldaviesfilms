a39f578b9232a4216f308734280a560b8115c989
JWLD <jackwldavies@icloud.com>
2017-09-12 17:58:12 +0100

    add basic express and handlebars setup

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const Express = require('express');
+const Hbs = require('express-handlebars');
+
+const app = Express();
+
+app.engine('hbs', Hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+// app.use(Express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+  console.log('Server running at 9000');
+});
