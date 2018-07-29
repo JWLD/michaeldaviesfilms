a45ff78fa127ae10739003ed38432fd7f2b1d660
JWLD <jackwldavies@icloud.com>
2018-07-29 19:11:50 +0100

    reorganise data

--- src/server.js
+++ src/server.js
@@ -1,29 +1,31 @@
-const Express = require('express');
-const Handlebars = require('express-handlebars');
-const { navCategories, scripts, portfolio } = require('./data.json');
+const Express = require('express')
+const Handlebars = require('express-handlebars')
+const { navCategories, portfolio } = require('./data.json')
 
-const app = Express();
+const app = Express()
 
-app.engine('hbs', Handlebars({
-  defaultLayout: 'main',
-  partialsPath: './partials',
-  extname: 'hbs'
-}));
+app.engine(
+  'hbs',
+  Handlebars({
+    defaultLayout: 'main',
+    partialsPath: './partials',
+    extname: 'hbs'
+  })
+)
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs')
 
-app.use(Express.static('public'));
+app.use(Express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('home', {
     navCategories,
-    scripts,
     portfolio
-  });
-});
+  })
+})
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000)
 
 app.listen(app.get('port'), () => {
-  console.log(`Server running at ${app.get('port')}`);
-});
+  console.log(`Server running at ${app.get('port')}`)
+})
